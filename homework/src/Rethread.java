4d1e014b9eea4d7a14b226e4e6db38c117ee23f1
Xujiangcheng <1781183680@qq.com>
2019-03-25 12:23:09 +0800

    demo

---
+++ homework/src/Rethread.java
@@ -0,0 +1,23 @@
+/**
+ * Created by alfred on 2019/3/24.
+ */
+public class Rethread extends Thread{
+    MainJpanel mainJpanel;
+    public Rethread(MainJpanel mainJpanel){
+        this.mainJpanel=mainJpanel;
+    }
+    @Override
+    public void run() {
+        while(true){
+            try {
+                Thread.sleep(200);
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+            mainJpanel.repaint();
+        }
+    }
+}
+
+
